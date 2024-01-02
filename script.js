a5b6984740f5789b4af77351538f7e00cca627f8
Amit Hariyale <57282097+Amithariyale@users.noreply.github.com>
2024-01-02 23:58:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 //your code here
+const images=document.querySelectorAll('img');
+let selectedImgs=[];
+
+for(let i=0;i<images.length;i++){
+	images[i].addEventListener('click',selectImg);
+}
+
+function selectImg(e) {
+	e.target.classList.add('selected');
+	selectedImgs.push(e.target.className);
+	if(selectedImgs.length===1){
+		const resetBtn=document.createElement('button');
+		resetBtn.id='reset';
+		resetBtn.innerText="RESET"
+		resetBtn.addEventListener('click',resetPage);
+		document.body.appendChild(resetBtn);
+	}
+}
+
+function resetPage() {
+	location.reload();
+}
+
